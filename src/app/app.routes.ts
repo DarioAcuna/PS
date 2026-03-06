553d2a8aa3aefa42d3cf5960afea01f381d8480c
xdarlo <dario.acuna101@alu.ulpgc.es>
2026-03-06 23:48:20 +0100

    login page and home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('../features/auth/pages/login/login').then(m => m.LoginComponent),
+  },
+];

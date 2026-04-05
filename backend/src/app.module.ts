28d6528a5cf66a3b74f50200439b877f6a8073b3
darlo <dario.acuna101@>
2026-04-05 01:19:54 +0100

    implementar CRUD de anuncios

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ClasesModule } from './clases/clases.module';
 import { HorariosModule } from './horarios/horarios.module';
 import { SesionesModule } from './sesiones/sesiones.module';
+import { AnunciosModule } from './anuncios/anuncios.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { SesionesModule } from './sesiones/sesiones.module';
     ClasesModule,
     HorariosModule,
     SesionesModule,
+    AnunciosModule,
   ],
   controllers: [AppController],
 })

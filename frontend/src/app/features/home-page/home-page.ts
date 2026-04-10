bb604786038fd3b0189d78bef97c8e9817d0cb06
Alvaro <alvarorodmi7@gmail.com>
2026-04-10 20:28:45 +0100

    First version HomePage Added

---
+++ frontend/src/app/features/home-page/home-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home-page.html',
+  styleUrls: ['./home-page.scss'],
+})
+
+export class HomePage {}
